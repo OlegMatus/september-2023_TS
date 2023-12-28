ca91e4a4221add3e4697af6cacf6a7003bd6415f
Oleg Matus <matys_bboy96@ukr.net>
2023-12-28 19:32:09 +0200

    hw10(task-6)

--- src/Lesson10(forms-ls)_TS/index.ts
+++ src/Lesson10(forms-ls)_TS/index.ts
@@ -1,38 +1,49 @@
 // Task-1 Створити форму з трьома полями для name, surname, age та кнопкою. При натисканні на кнопку зчитати дані з полів, та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
-interface IUser{
-    name: string;
-    surname: string;
-    age: number;
-}
-const form = document.forms['f1'] as HTMLFormElement;
-form.onsubmit = function (ev: Event) {
+// interface IUser{
+//     name: string;
+//     surname: string;
+//     age: number;
+// }
+// const form = document.forms['f1'] as HTMLFormElement;
+// form.onsubmit = function (ev: Event) {
+//     ev.preventDefault();
+//     const target = ev.target as HTMLFormElement
+//
+//     const obj: IUser = {
+//         name: target.username.value,
+//         surname: target.surname.value,
+//         age: parseInt(target.age.value),
+//     }
+//
+//     const div = document.createElement('div');
+//     const username = document.createElement('div');
+//     const surname = document.createElement('div');
+//     const age = document.createElement('div');
+//
+//     username.innerText = `Username: ${obj.name}`;
+//     surname.innerText = `Surname: ${obj.surname}`;
+//     age.innerText = `Age: ${obj.age}`;
+//
+//     div.classList.add('item', 'wrapper');
+//
+//     target.username.value = '';
+//     target.surname.value = '';
+//     target.age.value = '';
+//
+//     div.append(username, surname, age);
+//     document.body.appendChild(div);
+//
+//     const strObj = JSON.stringify(obj);
+//     div.innerText = `${strObj}`;
+// }
+// Task-6 Створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи менше він ніж 18, та повідомити про це користувача
+const form1 = document.forms['f2'] as HTMLFormElement;
+form1.addEventListener('submit',  function (ev: Event) {
     ev.preventDefault();
-    const target = ev.target as HTMLFormElement
-
-    const obj: IUser = {
-        name: target.username.value,
-        surname: target.surname.value,
-        age: parseInt(target.age.value),
-    }
-
-    const div = document.createElement('div');
-    const username = document.createElement('div');
-    const surname = document.createElement('div');
-    const age = document.createElement('div');
-
-    username.innerText = `Username: ${obj.name}`;
-    surname.innerText = `Surname: ${obj.surname}`;
-    age.innerText = `Age: ${obj.age}`;
-
-    div.classList.add('item', 'wrapper');
-
-    target.username.value = '';
-    target.surname.value = '';
-    target.age.value = '';
-
-    div.append(username, surname, age);
-    document.body.appendChild(div);
-
-    const strObj = JSON.stringify(obj);
-    div.innerText = `${strObj}`;
+    // const ageElement = document.querySelector('#ageInput input') as HTMLInputElement;
+    const ageElement = document.getElementById('ageInput') as HTMLInputElement;
+if (Number(ageElement.value) < 18) {
+    return alert('You are not 18 years old');
 }
+    return alert('Welcome');
+})
